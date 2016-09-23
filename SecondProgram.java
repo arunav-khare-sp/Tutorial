ad9f4ffe0c373bae6f44fec20024612f4d85c3b8
Arunav Khare <arunav.khare@sailpoint.com>
2016-09-23 15:59:48 +0530

    Added configuration file and new code in SecondProgam

--- SecondProgram.java
+++ SecondProgram.java
@@ -1,6 +1,13 @@
+// This is Just a class which represents
+// ScondProgram
+
 public class SecondProgram	{
 
+	private int memberInt;
+	private char memberChar;
+	
 	public SecondProgram()	{
-		
+		memberInt = 2;
+		memberChar = 'A';
 	}
 }
